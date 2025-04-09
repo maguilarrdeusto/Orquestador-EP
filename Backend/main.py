470a7163eb3237fab12fda4cd40baf2a48ea8ea8
maguilarrdeusto <martin.aguilar@opendeusto.es>
2025-04-09 11:48:32 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from Backend.models import EPRequest, EPResponse  
-from Backend.Orchestor import evaluar_ep
+from models import EPRequest, EPResponse  
+from Orchestor import evaluar_ep
 
 app = FastAPI()
 
